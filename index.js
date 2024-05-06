77d15ec5bd0e7ad740f7809ed3cd3508373dd5b0
JosephAJJames <130866326+JosephAJJames@users.noreply.github.com>
2024-05-06 21:03:33 +0000

    Fixed array currying

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ let warningLogger = logger("Warning");
 console.log(errorLogger("Syntax Error"));
 console.log(infoLogger("If statment"));
 console.log(warningLogger("Unused variable"));
+
+const arrApply = (f) => {
+  return (arr) => {
+    return arr.map((elem) => {
+      return f(elem);
+    });
+  }
+};
+
+const arrSquare = arrApply((x) => (x ** x));
+const arrDouble = arrApply((x) => (x * 2));
+
+console.log(arrSquare([1, 2, 3, 4]));
+console.log(arrDouble([1, 2, 3, 4]));
